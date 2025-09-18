d11ae70d730d019d127e3e604c22c01668e74c67
Fa1tloy <baranovdaniil534@gmail.com>
2025-09-18 09:15:17 +0300

    Добавлено выведение на консоль массива, второй массив, значения элементов которого содержат значения первого массива в обратном порядке и выведение на консоль второго массива

--- TestArray/TestArray/Program.cs
+++ TestArray/TestArray/Program.cs
@@ -16,6 +16,19 @@ namespace TestArray
             array[2] = "Как мимолетное видение, как гений чистой красоты";
 
             array[1] = "Передо мной преобразовалась в кинетическую энергию ты";
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+            
+           
+            string[] twoArray = new string[3];
+            twoArray[0] = array[2];
+            twoArray[1] = array[1];
+            twoArray[2] = array[0];
+
+            Console.WriteLine(twoArray[0]);
+            Console.WriteLine(twoArray[1]);
+            Console.WriteLine(twoArray[2]);
         }
     }
 }
