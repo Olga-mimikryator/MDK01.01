8f0b33eaf46e79c5c91c0d1c97b80e5f192c1f8f
ладно Омг <omg.ladno@bk.ru>
2025-10-13 00:16:40 +0300

    Создана функция Print; все созданные функции вызваны в основной программе

--- LR/LR2/LR2/Program.cs
+++ LR/LR2/LR2/Program.cs
@@ -8,10 +8,30 @@ namespace LR2
 {
     internal class Program
     {
+        static void Print(List<string> employees)
+        {
+            Console.WriteLine(String.Join(", ", employees));
+        }
+
         static void Main()
         {
             string[] employeeCategory = new string[] { "Инженеры", "Научный корпус", "Бухгалтерия" };
+            var (employees, salaries) = InputModule.InputByEmployeeCategory();
 
+            string userQuery = InputModule.InputUserQuery();
+
+            int indexCategory = SearchingModule.FindIndexCategory(userQuery, employeeCategory);
+            if (indexCategory < 0)
+            {
+                Console.WriteLine($"Какое еще {userQuery}? Не выделывайся и выбирай существующую категорию!");
+                return;
+            }
+
+            var (employeesUserCategory, salariesUserCategory) = SearchingModule.FindAllEmployeesByCategory(indexCategory, employees, salaries);
+
+            List <double> averageSalariesUserCategory = AnalysisDataModule.CalculateAverageSalaries(salariesUserCategory);
+            AnalysisDataModule.SortEmployeesBySalary(employeesUserCategory, averageSalariesUserCategory);
+            Print (employeesUserCategory);
         }
     }
 }
