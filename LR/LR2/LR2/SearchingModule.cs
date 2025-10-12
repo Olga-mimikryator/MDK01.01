876c1ecaed1f3e683a6d1d5532c689c820e24509
ладно Омг <omg.ladno@bk.ru>
2025-10-12 22:59:47 +0300

    Был создан класс SearchingModule и добаавлена функция FindIndexCategory для поиска индекса категории, которую выбрал пользователь

---
+++ LR/LR2/LR2/SearchingModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    internal class SearchingModule
+    {
+        static public int FindIndexCategory(string userQuery, string[] EmployeeCategory)
+        {
+            for (int index = 0; index < EmployeeCategory.Length; ++index)
+            {
+                string valueEmployeeCategory = EmployeeCategory[index].ToLower();
+                if (valueEmployeeCategory == userQuery.ToLower())
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
