51be712390f359c56148db111b4ce5c0903d1285
ладно Омг <omg.ladno@bk.ru>
2025-10-17 23:59:50 +0300

    Создана программа для определения четности числа пользователя с использованием перечисления enum

---
+++ enumUsing/enumUsing/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumUsing
+{
+    internal class Program
+    {
+
+        public enum EvenOrNotEven
+        {
+            четное,
+            нечетное
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            EvenOrNotEven result = (number % 2 == 0) ? EvenOrNotEven.четное :
+                                                       EvenOrNotEven.нечетное;
+
+            Console.WriteLine($"Ваше число {result}");
+        }
+    }
+}
