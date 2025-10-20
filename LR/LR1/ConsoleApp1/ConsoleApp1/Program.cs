1458aae3d2c3c2de97ca5b72d793a74e3224cd82
ладно Омг <omg.ladno@bk.ru>
2025-10-20 14:56:51 +0300

    Создан проект LR1 и начато выполнение первого задания: реализован код для введения пользователем длины массива userArray, элементов массива userArray и выведение получившегося массива userArray в консоль для наглядности

---
+++ LR/LR1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Вариант 7
+            ///Задание 1
+            
+            Console.Write("Введите желаемое количество элементов массива: ");
+            int lengthArray = Convert.ToInt32(Console.ReadLine());
+
+            double[] userArray = new double[lengthArray];
+            for (int i = 0; i < lengthArray; i++)
+            {
+                Console.Write($"Введите {i + 1} элемент: ");
+                userArray[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.Write("Получившийся массив: ");
+            Console.WriteLine(string.Join("; ", userArray));
+
+        }
+    }
+}
