1ce56939153031764272d59d7f2113cbedc32850
ладно Омг <omg.ladno@bk.ru>
2025-11-12 22:49:51 +0300

    Создана структура FuelRefill для хранения информации о процедурах заправки и внутренняя функция Show для вывода информации о процедуре из данной структуры

---
+++ Дистанционное_занятие/Дистанционное_занятие/FuelRefill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Дистанционное_занятие
+{
+
+    /// Струтура для информации о процедурах 
+    /// заправления машин топливом на заправке
+    public struct FuelRefill
+    {
+        public string carNumber;    ///номер машины
+        public int liters;          ///Количество литров топлива
+        public int cost;            ///Стоимость процедуры заправки
+        public int refillTime;      ///Время заправки машины
+
+        /// Функция вывода инфомарции о процедуре заправки
+        public void Show()
+        {
+            Console.WriteLine($"Машина: {carNumber} | Количество литров: {liters} | " +
+                              $"Стоимость: {cost} рублей | Время заправки: {refillTime} минут");
+        }
+    }
+}
