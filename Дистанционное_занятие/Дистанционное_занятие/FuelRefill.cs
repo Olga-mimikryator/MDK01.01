e3d3b7997eda4e533de46cabc9ba81c4c79ddda0
ладно Омг <omg.ladno@bk.ru>
2025-11-12 23:27:05 +0300

    Для структуры создан конструктор с параметрами. В основной программе создан список refills из структур FuelRefill. Написан код для автоматического заполнения списка refills случайными значенями.

--- Дистанционное_занятие/Дистанционное_занятие/FuelRefill.cs
+++ Дистанционное_занятие/Дистанционное_занятие/FuelRefill.cs
@@ -11,11 +11,23 @@ namespace Дистанционное_занятие
         public int cost;            ///Стоимость процедуры заправки
         public int refillTime;      ///Время заправки машины
 
+        ///Создание консктруктора для структуры, 
+        ///чтобы проще было заполнять данными
+        public FuelRefill(string number, int liters, int cost, int time)
+        {
+            this.carNumber = number;
+            this.liters = liters;
+            this.cost = cost;
+            this.refillTime = time;
+        }
+
         /// Функция вывода инфомарции о процедуре заправки
         public void Show()
         {
-            Console.WriteLine($"Машина: {carNumber} | Количество литров: {liters} | " +
-                              $"Стоимость: {cost} рублей | Время заправки: {refillTime} минут");
+            Console.WriteLine($"Машина: {carNumber} | " +
+                              $"Количество литров: {liters} | " +
+                              $"Стоимость: {cost} рублей | " +
+                              $"Время заправки: {refillTime} мин");
         }
     }
 }
--- Дистанционное_занятие/Дистанционное_занятие/Program.cs
+++ Дистанционное_занятие/Дистанционное_занятие/Program.cs
@@ -10,7 +10,21 @@ namespace Дистанционное_занятие
     {
         static void Main()
         {
+            /// Список из структур FuelRefill
+            List<FuelRefill> refills = new List<FuelRefill>();
+            
+            Random random = new Random();       
+            for (int i = 0; i < 101;  i++)      ///цикл для заполнения списка данными о
+            {                                   ///сотне структур FuelRefill
+                FuelRefill newrefill = new FuelRefill("A" + (100 + i).ToString(),   ///заполнение от А100 до А200
+                                                      random.Next(10, 71),          ///случайные числа от 10 до 70
+                                                      random.Next(900, 3001),       ///случайные числа от 900 до 3000
+                                                      random.Next(4, 21)            ///случайные числа от 4 до 20
+                );
 
+                refills.Add(newrefill);
+                ///refills[i].Show();
+            }
         }
     }
 }
