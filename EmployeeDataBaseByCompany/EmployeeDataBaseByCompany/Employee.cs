8232225a8372e4b765332fdb032a6c284f58b271
KateSmirnova1 <20katerina0060@gmail.com>
2025-12-03 08:57:56 +0300

    Создана программа EmployeeDataBaseByCompany для выполнения первого задания из категории 1. Объявлен абстрактный класс для объекета Employee с рядом обычных методов и абстрактного метода CalculateSalary, работа которого в последующем будет реализовываться

---
+++ EmployeeDataBaseByCompany/EmployeeDataBaseByCompany/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDataBaseByCompany
+{
+   public abstract class Employee
+    {
+        private int Id_;
+        private string FullName_;
+        private string Email_;
+
+        public void SetId(int Id)
+        {
+            Id_ = Id;
+        }
+        public int GetId()
+        {
+            return Id_;
+        }
+        public void SetFullName(string FullName)
+        {
+            FullName_ = FullName;
+        }
+        public string GetFullName()
+        {
+            return FullName_;
+        }
+        public void SetEmail(string Email)
+        {
+            Email_ = Email;
+        }
+        public string GetEmail()
+        {
+            return Email_;
+        }
+
+        public abstract decimal CalculateSalary();
+    }
+}
