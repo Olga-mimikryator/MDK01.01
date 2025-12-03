f44322bb95109d97398f810a6ccd0b1e4442a35a
KateSmirnova1 <20katerina0060@gmail.com>
2025-12-03 10:57:34 +0300

    Для класса Employee создан производный класс FreelancerEmployee для подсчета заработной платы работников за проект с учетом ндфл. Для каждого производного класса был создан тестовый объект класса Employee в основной программе и произведен вывод на консоль занесенных данных.

---
+++ EmployeeDataBaseByCompany/EmployeeDataBaseByCompany/FreelancerEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDataBaseByCompany
+{
+    public class FreelancerEmployee : Employee
+    {
+        private double PriceProject_;   ///плата за проект
+        private double Ndfl_;
+
+        public void SetPriceProject(double PriceProject)
+        {
+            PriceProject_ = PriceProject;
+        }
+        public void SetNdfl(double Ndfl)
+        {
+            Ndfl_ = Ndfl;
+        }
+
+        public override double CalculateSalary()
+        {
+            double SalaryBeforeNdfl = PriceProject_ - (PriceProject_ * Ndfl_ / 100);
+            return SalaryBeforeNdfl;
+        }
+    }
+}
--- EmployeeDataBaseByCompany/EmployeeDataBaseByCompany/Program.cs
+++ EmployeeDataBaseByCompany/EmployeeDataBaseByCompany/Program.cs
@@ -10,7 +10,30 @@ namespace EmployeeDataBaseByCompany
     {
         static void Main()
         {
+            FixedSalaryEmployee electrician = new FixedSalaryEmployee();
+            electrician.SetId(1);
+            electrician.SetFullName("Пикачукин Анатолий Степанович");
+            electrician.SetEmail("pikachuchukin.as@gmail.com");
+            electrician.SetSalaryMonth(43560);
+            Console.WriteLine($"Заработная плата электрика {electrician.GetFullName()} составляет {electrician.CalculateSalary()} рублей");
 
+            HourlySalaryEmployee tester = new HourlySalaryEmployee();
+            tester.SetId(2);
+            tester.SetFullName("БаговНет Анастасия Юрьевна");
+            tester.SetEmail("bagov.nothave.au@mail.ru");
+            tester.SetSalaryByOneHourWork(500.05);
+            tester.SetHoursWork(110);
+            tester.SetOvertimeHours(20);
+            tester.SetOvertimeSalary(1.5);
+            Console.WriteLine($"Заработная плата тестера {tester.GetFullName()} составляет {tester.CalculateSalary()} рублей");
+
+            FreelancerEmployee programmist = new FreelancerEmployee();
+            programmist.SetId(3);
+            programmist.SetFullName("НейронкиПриРазработкеНеИспользович Владимир Александрович");
+            programmist.SetEmail("ai.slop.notfound@gmail.com");
+            programmist.SetPriceProject(80000);
+            programmist.SetNdfl(1.3);
+            Console.WriteLine($"Заработная плата программиста {programmist.GetFullName()} составляет {programmist.CalculateSalary()} рублей");
         }
     }
 }
